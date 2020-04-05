a90d408b97978a1f9ffbefa5ee25f92dc8ac66dc
RaQu <raquintero.dg@outlook.com>
2020-04-04 22:40:17 -0500

    Platzi Events

---
+++ prog_basica/events-form/event.js
@@ -0,0 +1,9 @@
+let actual = 2080;
+let nacimiento = prompt();
+
+let calcularEdad = function(){
+    edad = actual - nacimiento;
+    //document.write(edad);
+    return(edad);
+};
+console.log(calcularEdad());

34bd78b24a96bf6b1b9647111a52a35b988ac3a8
raquinterob <raulandresquinterob@outlook.com>
2022-08-06 18:22:10 -0500

    dibujo5.html complete -> draw with mouse

--- canvas/dibujo5.js
+++ canvas/dibujo5.js
@@ -1,23 +1,38 @@
-let teclas = {
-  LEFT: 37,
-  UP: 38,
-  RIGHT: 39,
-  DOWN: 40,
-}
-
-document.addEventListener('keydown', dibujoPorTecla)
-
-let cuadrito = document.getElementById('area_de_dibujo')
-let papel = cuadrito.getContext('2d')
-let xLinea = cuadrito.width / 2
-let yLinea = cuadrito.height / 2
-let i = 0
+let canva = document.getElementById('area_de_dibujo')
+let lienzo = canva.getContext('2d')
+let canvaWidth = canva.width
+let canvaHeight = canva.height
+let canvaPositionLeft = canva.offsetLeft
+let canvaPositionTop = canva.offsetTop
 let trazoLinea = 3
-let movimiento = 5
-let hueChange = 3
+let i = 0
+let hueChange = 9
 let colorLinea = 'hsl(' + i + ',100%,50%)'
 
-dibujarLinea(colorLinea, xLinea - 1, yLinea - 1, xLinea + 1, yLinea + 1, papel)
+let clickInicialX
+let clickInicialY
+
+function dibujarOnClickInicial(evento) {
+  clickInicialX = evento.clientX - canvaPositionLeft
+  clickInicialY = evento.clientY - canvaPositionTop
+  return
+
+}
+
+function dibujarOnClickFinal(evento) {
+  clickFinalX = evento.clientX - canvaPositionLeft
+  clickFinalY = evento.clientY - canvaPositionTop
+
+  colorLinea = 'hsl(' + i + ',100%,50%)'
+
+  dibujarLinea(colorLinea, clickInicialX, clickInicialY, clickFinalX, clickFinalY, lienzo)
+  i += hueChange
+  //console.log(evento)
+  //console.log(canva)
+}
+
+canva.addEventListener('mousedown', dibujarOnClickInicial)
+canva.addEventListener('mouseup', dibujarOnClickFinal)
 
 function dibujarLinea(colorLinea, xinicial, yinicial, xfinal, yfinal, lienzo) {
   lienzo.beginPath()
@@ -27,76 +42,4 @@ function dibujarLinea(colorLinea, xinicial, yinicial, xfinal, yfinal, lienzo) {
   lienzo.lineTo(xfinal, yfinal)
   lienzo.stroke()
   lienzo.closePath()
-}
-
-function dibujoPorTecla(evento) {
-  //console.log('Tecla oprimida =' + evento.keyCode + ' ' + evento.key)
-  //console.log(evento)
-  //if (evento.keyCode == teclas.LEFT) {
-  //  console.log("Vamo'pa arriba")
-  //}
-
-  switch (evento.keyCode) {
-    case teclas.LEFT:
-      //console.log("Vamo'pa Izquierda")
-      //console.log(xLinea)
-      i += hueChange
-      colorLinea = 'hsl(' + i + ',100%,50%)'
-      dibujarLinea(
-        colorLinea,
-        xLinea,
-        yLinea,
-        xLinea - movimiento,
-        yLinea,
-        papel
-      )
-      xLinea = xLinea - movimiento
-      break
-    case teclas.UP:
-      //console.log('Vamo Arriba')
-      i += hueChange
-      colorLinea = 'hsl(' + i + ',100%,50%)'
-      dibujarLinea(
-        colorLinea,
-        xLinea,
-        yLinea,
-        xLinea,
-        yLinea - movimiento,
-        papel
-      )
-      yLinea = yLinea - movimiento
-      break
-    case teclas.RIGHT:
-      //console.log("Vamo'pa Derecha")
-      i += hueChange
-      colorLinea = 'hsl(' + i + ',100%,50%)'
-      dibujarLinea(
-        colorLinea,
-        xLinea,
-        yLinea,
-        xLinea + movimiento,
-        yLinea,
-        papel
-      )
-      xLinea = xLinea + movimiento
-      break
-    case teclas.DOWN:
-      //console.log('Vamo abajo')
-      i += hueChange
-      colorLinea = 'hsl(' + i + ',100%,50%)'
-      dibujarLinea(
-        colorLinea,
-        xLinea,
-        yLinea,
-        xLinea,
-        yLinea + movimiento,
-        papel
-      )
-      yLinea = yLinea + movimiento
-      break
-
-    default:
-      //console.log('otra tecla')
-      break
-  }
 }

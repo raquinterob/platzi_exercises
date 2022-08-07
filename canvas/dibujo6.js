b7cf7acdef63e857be491d3c39296f044d189b5f
raquinterob <raulandresquinterob@outlook.com>
2022-08-06 19:13:36 -0500

    Change line color dibujo6

--- canvas/dibujo6.js
+++ canvas/dibujo6.js
@@ -1,26 +1,28 @@
 //======================================================================
 // VARIABLES
 //======================================================================
-let miCanvas = document.querySelector('#area_de_dibujo');
-let lineas = [];
-let correccionX = 0;
-let correccionY = 0;
-let pintarLinea = false;
+let miCanvas = document.querySelector('#area_de_dibujo')
+let lineas = []
+let correccionX = 0
+let correccionY = 0
+let pintarLinea = false
 
 // Marca el nuevo punto
-let nuevaPosicionX = 0;
-let nuevaPosicionY = 0;
+let nuevaPosicionX = 0
+let nuevaPosicionY = 0
 
 let posicion = miCanvas.getBoundingClientRect()
 // correccionX = posicion.x;
 // correccionY = posicion.y;
-correccionX = posicion.x;
-correccionY = posicion.y;
+correccionX = posicion.x
+correccionY = posicion.y
 
-miCanvas.width = 600;
-miCanvas.height = 600;
+// Color de Linea
+let i = 0
+let hueChange = 9
 
-console.log(miCanvas.offsetLeft);
+miCanvas.width = 600
+miCanvas.height = 600
 
 //======================================================================
 // FUNCIONES
@@ -30,63 +32,66 @@ console.log(miCanvas.offsetLeft);
  * Funcion que empieza a dibujar la linea
  */
 function empezarDibujo() {
-    pintarLinea = true;
-    lineas.push([]);
-    console.log(nuevaPosicionX);
-};
+  pintarLinea = true
+  lineas.push([])
+  console.log(nuevaPosicionX)
+}
 
 /**
  * Funcion que guarda la posicion de la nueva línea
  */
 function guardarLinea() {
-    lineas[lineas.length - 1].push({
-        x: nuevaPosicionX - miCanvas.offsetLeft,
-        y: nuevaPosicionY - miCanvas.offsetTop
-    });
+  lineas[lineas.length - 1].push({
+    x: nuevaPosicionX - miCanvas.offsetLeft,
+    y: nuevaPosicionY - miCanvas.offsetTop,
+  })
 }
 
 /**
  * Funcion dibuja la linea
  */
 function dibujarLinea(event) {
-    event.preventDefault();
-    if (pintarLinea) {
-        let ctx = miCanvas.getContext('2d')
-        // Estilos de linea
-        ctx.lineJoin = ctx.lineCap = 'round';
-        ctx.lineWidth = 4;
-        // Color de la linea
-        ctx.strokeStyle = '#fff';
-        // Marca el nuevo punto
-        if (event.changedTouches == undefined) {
-            // Versión ratón
-            nuevaPosicionX = event.layerX;
-            nuevaPosicionY = event.layerY;
-        } else {
-            // Versión touch, pantalla tactil
-            nuevaPosicionX = event.changedTouches[0].pageX - correccionX;
-            nuevaPosicionY = event.changedTouches[0].pageY - correccionY;
-        }
-        // Guarda la linea
-        guardarLinea();
-        // Redibuja todas las lineas guardadas
-        ctx.beginPath();
-        lineas.forEach(function (segmento) {
-            ctx.moveTo(segmento[0].x, segmento[0].y);
-            segmento.forEach(function (punto, index) {
-                ctx.lineTo(punto.x, punto.y);
-            });
-        });
-        ctx.stroke();
+  event.preventDefault()
+  if (pintarLinea) {
+    let ctx = miCanvas.getContext('2d')
+    // Estilos de linea
+    ctx.lineJoin = ctx.lineCap = 'round'
+    ctx.lineWidth = 4
+    // Color de la linea
+    // ctx.strokeStyle = '#fff';
+    ctx.strokeStyle = 'hsl(' + i + ',100%,50%)'
+    i += hueChange
+
+    // Marca el nuevo punto
+    if (event.changedTouches == undefined) {
+      // Versión ratón
+      nuevaPosicionX = event.layerX
+      nuevaPosicionY = event.layerY
+    } else {
+      // Versión touch, pantalla tactil
+      nuevaPosicionX = event.changedTouches[0].pageX - correccionX
+      nuevaPosicionY = event.changedTouches[0].pageY - correccionY
     }
+    // Guarda la linea
+    guardarLinea()
+    // Redibuja todas las lineas guardadas
+    ctx.beginPath()
+    lineas.forEach(function (segmento) {
+      ctx.moveTo(segmento[0].x, segmento[0].y)
+      segmento.forEach(function (punto, index) {
+        ctx.lineTo(punto.x, punto.y)
+      })
+    })
+    ctx.stroke()
+  }
 }
 
 /**
  * Funcion que deja de dibujar la linea
  */
-function pararDibujar () {
-    pintarLinea = false;
-    guardarLinea();
+function pararDibujar() {
+  pintarLinea = false
+  guardarLinea()
 }
 
 //======================================================================
@@ -94,10 +99,10 @@ function pararDibujar () {
 //======================================================================
 
 // Eventos raton
-miCanvas.addEventListener('mousedown', empezarDibujo, false);
-miCanvas.addEventListener('mousemove', dibujarLinea, false);
-miCanvas.addEventListener('mouseup', pararDibujar, false);
+miCanvas.addEventListener('mousedown', empezarDibujo, false)
+miCanvas.addEventListener('mousemove', dibujarLinea, false)
+miCanvas.addEventListener('mouseup', pararDibujar, false)
 
 // Eventos pantallas táctiles
-miCanvas.addEventListener('touchstart', empezarDibujo, false);
-miCanvas.addEventListener('touchmove', dibujarLinea, false);
+miCanvas.addEventListener('touchstart', empezarDibujo, false)
+miCanvas.addEventListener('touchmove', dibujarLinea, false)

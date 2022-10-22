caf121aa36f58b58c49ec03409f9ea63068b479a
raquinterob <raulandresquinterob@outlook.com>
2022-10-21 20:51:22 -0500

    Reto move scarecrow with keys

--- js/math-game-1.js
+++ js/math-game-1.js
@@ -1,36 +1,177 @@
 let d = document.getElementById('map-area')
 let lienzo = d.getContext('2d')
-let mapa = '/images/tile.png'
 
-var fondo = new Image()
-fondo.src = mapa
-fondo.addEventListener('load', dibujar)
-
-function dibujar() {
-  lienzo.drawImage(fondo, 0, 0)
+let fondo = {
+  url: '/images/tile.png',
+  cargaOK: false,
 }
 
-let vaca = new Image()
-vaca.src = 'images/vaca.png'
-vaca.addEventListener('load', dibujarVacas)
+let vaca = {
+  url: 'images/vaca.png',
+  cargaOK: false,
+  cantidad: aleatorio(1, 4),
+  x: aleatorio(0, 420),
+  y: aleatorio(0, 420),
+}
 
+let cerdo = {
+  url: '/images/cerdo.png',
+  cargaOK: false,
+  cantidad: aleatorio(1, 6),
+  x: aleatorio(0, 420),
+  y: aleatorio(0, 420),
+}
+
+let pollo = {
+  url: 'images/pollo.png',
+  cargaOK: false,
+  cantidad: aleatorio(1, 8),
+  x: aleatorio(0, 420),
+  y: aleatorio(0, 420),
+}
+
+let scarecrow = {
+  url: 'images/scarecrow.png',
+  cargaOK: false,
+  x: aleatorio(0, 420),
+  y: aleatorio(0, 420),
+}
+
+fondo.imagen = new Image()
+fondo.imagen.src = fondo.url
+fondo.imagen.addEventListener('load', cargarFondo)
+
+vaca.imagen = new Image()
+vaca.imagen.src = vaca.url
+vaca.imagen.addEventListener('load', cargarVacas)
+
+cerdo.imagen = new Image()
+cerdo.imagen.src = cerdo.url
+cerdo.imagen.addEventListener('load', cargarCerdos)
+
+pollo.imagen = new Image()
+pollo.imagen.src = pollo.url
+pollo.imagen.addEventListener('load', cargarPollos)
+
+scarecrow.imagen = new Image()
+scarecrow.imagen.src = scarecrow.url
+scarecrow.imagen.addEventListener('load', cargarScarecrow)
+
+function cargarFondo() {
+  fondo.cargaOK = true
+  dibujar()
+}
+
+function cargarVacas() {
+  vaca.cargaOK = true
+  dibujar()
+}
+
+function cargarCerdos() {
+  cerdo.cargaOK = true
+  dibujar()
+}
+
+function cargarPollos() {
+  pollo.cargaOK = true
+  dibujar()
+}
+
+function cargarScarecrow() {
+  scarecrow.cargaOK = true
+  dibujar()
+}
+
+function dibujar() {
+  if (
+    fondo.cargaOK &&
+    vaca.cargaOK &&
+    cerdo.cargaOK &&
+    pollo.cargaOK &&
+    scarecrow.cargaOK
+  ) {
+    lienzo.drawImage(fondo.imagen, 0, 0)
+
+    for (let i = 0; i < vaca.cantidad; i++) {
+      let x = aleatorio(0, 420)
+      let y = aleatorio(0, 420)
+      lienzo.drawImage(vaca.imagen, vaca.x, vaca.y)
+    }
+
+    for (let i = 0; i < cerdo.cantidad; i++) {
+      let x = aleatorio(0, 420)
+      let y = aleatorio(0, 420)
+      lienzo.drawImage(cerdo.imagen, cerdo.x, cerdo.y)
+    }
+
+    for (let i = 0; i < pollo.cantidad; i++) {
+      let x = aleatorio(0, 420)
+      let y = aleatorio(0, 420)
+      lienzo.drawImage(pollo.imagen, pollo.x, pollo.y)
+    }
+
+    console.log(
+      'vacas= ' +
+        vaca.cantidad +
+        ' | cerdos= ' +
+        cerdo.cantidad +
+        ' | pollos= ' +
+        pollo.cantidad
+    )
+
+    lienzo.drawImage(scarecrow.imagen, scarecrow.x, scarecrow.y)
+    //console.log('scarecrow.x=' + scarecrow.x + ' scarecrow.y=' + scarecrow.y)
+  }
+}
+
+let movimiento = 10
+let teclas = {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+}
+
+document.addEventListener('keydown', moverScarecrow)
+
+function moverScarecrow(evento) {
+  //console.log('Tecla oprimida =' + evento.keyCode + ' ' + evento.key)
+
+  switch (evento.keyCode) {
+    case teclas.LEFT:
+      scarecrow.x = scarecrow.x - movimiento
+      dibujar()
+      break
+    case teclas.RIGHT:
+      scarecrow.x = scarecrow.x + movimiento
+      dibujar()
+      break
+    case teclas.UP:
+      scarecrow.y = scarecrow.y - movimiento
+      dibujar()
+      break
+    case teclas.DOWN:
+      scarecrow.y = scarecrow.y + movimiento
+      dibujar()
+      break
+    default:
+      console.log('otra tecla')
+      break
+  }
+}
+
+// Primer Opcion dibujando uno por uno, pero con posibles problemas de
+// orden de dibujo (capas), esposible que el fondo quede encima de todo
+/*
 let cerdo = new Image()
 cerdo.src = 'images/cerdo.png'
-cerdo.addEventListener('load', dibujarCerdos)
+cerdo.addEventListener('load', cargarCerdos)
 
 let pollo = new Image()
 pollo.src = 'images/pollo.png'
-pollo.addEventListener('load', dibujarPollos)
+pollo.addEventListener('load', cargarPollos)
 
-function dibujarVacas() {
-  let cantidad = aleatorio(1, 4)
-  console.log('# de vacas = ' + cantidad)
-
-  for (let i = 0; i < cantidad; i++) {
-    lienzo.drawImage(vaca, aleatorio(0, 420), aleatorio(0, 420))
-  }
-}
-function dibujarCerdos() {
+function cargarCerdos() {
   let cantidad = aleatorio(1, 4)
   console.log('# de cerdos = ' + cantidad)
 
@@ -38,7 +179,7 @@ function dibujarCerdos() {
     lienzo.drawImage(cerdo, aleatorio(0, 420), aleatorio(0, 420))
   }
 }
-function dibujarPollos() {
+function cargarPollos() {
   let cantidad = aleatorio(1, 4)
   console.log('# de pollos = ' + cantidad)
 
@@ -46,6 +187,7 @@ function dibujarPollos() {
     lienzo.drawImage(pollo, aleatorio(0, 420), aleatorio(0, 420))
   }
 }
+*/
 
 // Random Numbers - Images position
 function aleatorio(min, maxi) {

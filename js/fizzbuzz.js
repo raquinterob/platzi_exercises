43074f7cc1e028cdef39be6e1280b773901ca839
raquinterob <raulandresquinterob@outlook.com>
2022-10-27 21:23:33 -0500

    Update fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,12 +1,7 @@
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    respuesta = i + '. fizzbuzz'
-  } else if (i % 3 == 0) {
-    respuesta = i + '. fizz'
-  } else if (i % 5 == 0) {
-    respuesta = i + '. buzz'
-  } else {
-    respuesta = '<span style="color: gray;">' + i + '.</span>'
-  }
+  if (i % 3 == 0 && i % 5 == 0) respuesta = i + '. fizzbuzz'
+  else if (i % 3 == 0) respuesta = i + '. fizz'
+  else if (i % 5 == 0) respuesta = i + '. buzz'
+  else respuesta = '<span style="color: gray;">' + i + '.</span>'
   document.write(respuesta + '<br />')
 }

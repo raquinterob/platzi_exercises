f022b07512623fc8c58309fac15fc78fe1688845
RaQ <raq@PM.local>
2022-11-28 19:03:47 -0500

    Update Pakidesk - load each info and image

---
+++ js/classes-arrays-game.js
@@ -0,0 +1,22 @@
+class Pakiman {
+  constructor(n, v, a, t) {
+    this.nombre = n
+    this.vida = v
+    this.ataque = a
+    this.tipo = t
+    this.imagen = new Image()
+    this.imagen.src = imagenes[this.nombre]
+  }
+  hablar() {
+    alert(this.nombre)
+  }
+  mostrar() {
+    document.getElementById("mapa").appendChild(this.imagen)
+    document.write('<div class="pakiman-data">')
+    document.write('<p><strong>' + this.nombre + '</strong><br/>')
+    document.write('<i>Salud = </i>' + this.vida + '<br/>')
+    document.write('<i>Ataque = </i>' + this.ataque + '<br/>')
+    document.write('<i>Tipo = </i>' + this.tipo + '<hr></p>')
+    document.write('</div>')
+  }
+}
---
+++ js/classes-arrays-pakiman.js
@@ -0,0 +1,19 @@
+const imagenes = []
+imagenes['Cauchin'] = '//resources.rqdigital.co/img/granja/vaca.png'
+imagenes['Pokacho'] = '//resources.rqdigital.co/img/granja/pollo.png'
+imagenes['Tocinauro'] = '//resources.rqdigital.co/img/granja/cerdo.png'
+
+/*tocinauro.mostrar()
+pokacho.mostrar()
+cauchin.mostrar()*/
+
+let coleccion = []
+coleccion.push(new Pakiman('Cauchin', 100, 30, 'Tierra'))
+coleccion.push(new Pakiman('Pokacho', 80, 50, 'Aire'))
+coleccion.push(new Pakiman('Tocinauro', 120, 40, 'Magia'))
+
+console.log(coleccion)
+
+for (let i of coleccion) { //in itera el indice - of itera obejtos
+  i.mostrar()
+}

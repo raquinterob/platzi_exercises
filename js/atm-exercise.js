74456d0bc1142574514b38bf0197d5bc985906e4
RaQ <raq@PM.local>
2022-12-06 12:25:14 -0500

    ATM - update variables

--- js/atm-exercise.js
+++ js/atm-exercise.js
@@ -3,6 +3,55 @@ let c50 = 3
 let c20 = 2
 let c10 = 2
 
+let btnRetirar = document.getElementById('btnRetirar')
+btnRetirar.addEventListener('click', retirar)
+
+function retirar() {
+  let retiro = document.getElementById('retiro').value
+  let respuesta = document.getElementById('respuesta')
+  let mensaje
+  let totalParcial = retiro
+
+  document.getElementById('solicitado').innerText = '$' + totalParcial
+
+  let b50 = parseInt(retiro / 50)
+  if (c50 < b50) {
+    b50 = c50
+  }
+  totalParcial = retiro - b50 * 50
+  console.log(b50 + ' Billetes de 50 // Restante ' + totalParcial)
+
+  let b20 = parseInt(totalParcial / 20)
+  if (c20 < b20) {
+    b20 = c20
+  }
+  totalParcial = totalParcial - b20 * 20
+  console.log(b20 + ' Billetes de 20 // Restante ' + totalParcial)
+
+  let b10 = parseInt(totalParcial / 10)
+  totalParcial = totalParcial - b10 * 10
+  if (c10 < b10) {
+    b10 = c10
+    console.log(b10 + ' Billetes de 10 // Restante ' + totalParcial)
+    mensaje = 'No tengo suficiente dinero'
+  } else if (totalParcial % 10 !== 0) {
+    mensaje = 'No tengo billetes de baja denominación, solo múltiplos de 10'
+  } else {
+    console.log(b10 + ' Billetes de 10 // Restante ' + totalParcial)
+    mensaje = 'Toma tu platica!! $' + retiro
+  }
+
+  respuesta.innerText = mensaje
+  console.log('-----')
+
+}
+
+/*
+//Disponibilidad de Billetes del ATM
+let c50 = 3
+let c20 = 2
+let c10 = 2
+
 //let usuario = parseInt(prompt('Cuanto quieres retirar?'))
 let btnRetirar = document.getElementById('btnRetirar')
 btnRetirar.addEventListener('click', retirar)
@@ -42,3 +91,4 @@ function retirar() {
 
   console.log('-----')
 }
+*/
